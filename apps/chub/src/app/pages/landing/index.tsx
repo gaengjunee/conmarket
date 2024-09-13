891e006ae6c3779a24c730ebc88c807a455188f8
gaengjunee <soosungkj@gmail.com>
2024-09-13 10:31:12 +0900

    qwe

--- apps/chub/src/app/pages/landing/index.tsx
+++ apps/chub/src/app/pages/landing/index.tsx
@@ -1,10 +1,10 @@
 import { LandingLayout } from '@/components';
-
+import { Button, Icon123 } from '@repo/ui';
 
 function Landing() {
   return (
     <LandingLayout w='100vw' h='100vh' >
-      Landing
+      <Button  leftSection={<Icon123/>} >Landing</Button>
     </LandingLayout>
   )
 }
